3b81977b21153ae02f1a94c3b05bf3bd462414f7
antonbulybenko-cell <anton.bulybenko@gmail.com>
2026-05-13 14:13:47 +0000

    Вправа 27

--- vprava27.py
+++ vprava27.py
@@ -1,6 +1,17 @@
 from turtle import *
+speed(1)
 
 n = int(input('Введідь кількість кутів: '))
+
+if n == 1:
+  dot(10)
+  circle(50)
+
+if n == 2:
+  dot(10)
+  circle(50, 180)
+  dot(10)
+  circle(50, 180)
 
 if n == 3:
   forward(50)
@@ -10,4 +21,45 @@ if n == 3:
   forward(50)
   right(120)
 
+if n == 4:
+  forward(50)
+  right(90)
+  forward(50)
+  right(90)
+  forward(50)
+  right(90)
+  forward(50)
+  right(90)
+
+if n == 5:
+  forward(50)
+  right(72)
+  forward(50)
+  right(72)
+  forward(50)
+  right(72)
+  forward(50)
+  right(72)
+  forward(50)
+  right(72)
+
+if n == 6:
+  forward(50)
+  right(60)
+  forward(50)
+  right(60)
+  forward(50)
+  right(60)
+  forward(50)
+  right(60)
+  forward(50)
+  right(60)
+  forward(50)
+  right(60)
+
+if n >= 7:
+  for i in range(n):
+    forward(50-n/3)
+    right(360/n)
+  
 done()
